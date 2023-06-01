00ccdc4546183660d46932b29ea84ab53db3eab9
liubiyong <liubiyongge@163.com>
2023-06-01 09:54:58 +0800

    fix bugs avoid threshold less 0

--- fs/fs_zenfs.h
+++ fs/fs_zenfs.h
@@ -467,7 +467,7 @@ class ZenFS : public FileSystemWrapper {
 
  private:
   const uint64_t GC_START_LEVEL =
-      30;                      /* Enable GC when < 20% free space available */
+      20;                      /* Enable GC when < 20% free space available */
   const uint64_t GC_SLOPE = 3; /* GC agressiveness */
   void GCWorker();
 };
